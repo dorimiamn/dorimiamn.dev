c3b30285e0abd14f403b75796ca195981434f3e1
dorimiamn <dorimiamn@gmail.com>
2025-02-10 05:22:35 +0900

    renderToString を追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { renderToString } from "hono/jsx/dom/server";
 
 const app = new Hono();
 
@@ -10,30 +11,31 @@ app.get("/api/clock", (c) => {
 
 app.get("/", (c) => {
 	return c.html(
-		<html lang="ja">
-			<head>
-				<link href="/static/style.css" rel="stylesheet" />
-				{import.meta.env.PROD ? (
-					<link rel="stylesheet" href="/static/style.css" />
-				) : (
-					<link rel="stylesheet" href="/src/style.css" />
-				)}
-				<link
-					rel="shortcut icon"
-					href="/static/favicon.ico"
-					type="image/x-icon"
-				/>
-				<title>Dorimiamn's Site</title>
-			</head>
-			<body>
-				<div id="root" />
-				{import.meta.env.PROD ? (
-					<script type="module" src="/static/client.js" />
-				) : (
-					<script type="module" src="/src/client.tsx" />
-				)}
-			</body>
-		</html>,
+		renderToString(
+			<html lang="ja">
+				<head>
+					{import.meta.env.PROD ? (
+						<link rel="stylesheet" href="/static/style.css" />
+					) : (
+						<link rel="stylesheet" href="/src/style.css" />
+					)}
+					<link
+						rel="shortcut icon"
+						href="/static/favicon.ico"
+						type="image/x-icon"
+					/>
+					<title>Dorimiamn's Site</title>
+				</head>
+				<body>
+					<div id="root" />
+					{import.meta.env.PROD ? (
+						<script type="module" src="/static/client.js" />
+					) : (
+						<script type="module" src="/src/client.tsx" />
+					)}
+				</body>
+			</html>,
+		),
 	);
 });
 
