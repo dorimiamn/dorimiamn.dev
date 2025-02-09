14e510b3c616b0c91377a17adfb18e06672cd425
dorimiamn <dorimiamn@gmail.com>
2025-02-09 16:21:32 +0900

    クライアントサイドのコードを実行できるように構成を変更

---
+++ src/client.tsx
@@ -0,0 +1,30 @@
+import { useState } from "hono/jsx/dom";
+import { createRoot } from "hono/jsx/dom/client";
+
+function App() {
+	return (
+		<>
+			<h1>Hello, Hono with JSX!</h1>
+			<h2>Example of useState()</h2>
+			<Counter />
+			<h2>Example of API fetch()</h2>
+		</>
+	);
+}
+
+function Counter() {
+	const [count, setCount] = useState(0);
+	return (
+		<div>
+			<p>Count: {count}</p>
+			<button type="button" onClick={() => setCount(count + 1)}>
+				Increment
+			</button>
+		</div>
+	);
+}
+
+// biome-ignore lint/style/noNonNullAssertion: DOM ノードは存在するはずなので
+const domNode = document.getElementById("root")!;
+const root = createRoot(domNode);
+root.render(<App />);
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
 import { Hono } from "hono";
-import { renderer } from "./renderer";
 
 const app = new Hono();
 
-app.use(renderer);
+app.get("/api/clock", (c) => {
+	return c.json({
+		time: new Date().toLocaleTimeString(),
+	});
+});
 
 app.get("/", (c) => {
-	return c.render(<h1>Hello!</h1>);
+	return c.html(
+		<html lang="ja">
+			<head>
+				<link href="/static/style.css" rel="stylesheet" />
+				<title>Dorimiamn's Site</title>
+			</head>
+			<body>
+				<div id="root" />
+				{import.meta.env.PROD ? (
+					<script type="module" src="/static/client.js" />
+				) : (
+					<script type="module" src="/src/client.tsx" />
+				)}
+			</body>
+		</html>,
+	);
 });
 
 export default app;
