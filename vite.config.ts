2ff4ffb58b3e8fbdb9a9330ece8333c584037972
dorimiamn <dorimiamn@gmail.com>
2025-02-11 00:11:02 +0900

    バンドル後の css ファイル名を指定した

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
 					output: {
 						dir: "./dist/static",
 						entryFileNames: "client.js",
+						assetFileNames: "style.css",
 					},
 				},
 				copyPublicDir: false,
