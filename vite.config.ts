a1ef9a7ba08fb3791de68722951e85be9638a901
dorimiamn <dorimiamn@gmail.com>
2025-02-10 23:29:07 +0900

    ビルド時に client.js が生成されるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,29 @@ import adapter from "@hono/vite-dev-server/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-	plugins: [
-		build(),
-		devServer({
-			adapter,
-			entry: "src/index.tsx",
-		}),
-		tailwindcss(),
-	],
+export default defineConfig(({ mode }) => {
+	if (mode === "client") {
+		return {
+			build: {
+				rollupOptions: {
+					input: "./src/client.tsx",
+					output: {
+						dir: "./dist/static",
+						entryFileNames: "client.js",
+					},
+				},
+				copyPublicDir: false,
+			},
+		};
+	}
+	return {
+		plugins: [
+			build(),
+			devServer({
+				adapter,
+				entry: "src/index.tsx",
+			}),
+			tailwindcss(),
+		],
+	};
 });
