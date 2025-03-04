b7b535b4286939f6dea38c87fedf2dcf2d0c9d27
dorimiamn <dorimiamn@gmail.com>
2025-03-04 20:04:35 +0900

    SSG 処理を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import build from "@hono/vite-build/cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
 import adapter from "@hono/vite-dev-server/cloudflare";
+import ssg from "@hono/vite-ssg";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
 	if (mode === "client") {
 		return {
-			plugins: [tailwindcss()],
+			plugins: [tailwindcss(), ssg()],
 			build: {
 				rollupOptions: {
 					input: ["./src/client.tsx", "./src/style.css"],
@@ -24,6 +25,7 @@ export default defineConfig(({ mode }) => {
 	return {
 		plugins: [
 			build(),
+			ssg(),
 			devServer({
 				adapter,
 				entry: "src/index.tsx",
